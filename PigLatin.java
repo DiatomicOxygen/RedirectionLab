58baf904bde20d8e59d26850cd4d35c9feb82450
DiatomicOxygen <>
2021-01-02 17:01:18 -0500

    pigLatin method works for 1 character words

--- PigLatin.java
+++ PigLatin.java
@@ -16,13 +16,16 @@ public class PigLatin {
   public static String pigLatin(String s) {
     String ans = "";
     String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
-
-    for (String diagraph : diagraphs) {
-      if (s.substring(0,2).equals(diagraph)) {
-        ans = s.substring(2) + diagraph + "ay";
-        return ans;
+    if (s.length() > 1) {
+      for (String diagraph : diagraphs) {
+        if (s.substring(0,2).equals(diagraph)) {
+          ans = s.substring(2) + diagraph + "ay";
+          return ans;
+        }
       }
     }
     return pigLatinSimple(s);
   }
+
+
 }
