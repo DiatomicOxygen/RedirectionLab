20241b924df6139775bb894872f43e89262d2b9e
DiatomicOxygen <>
2021-01-02 16:05:59 -0500

    MakeStars works

--- MakeStars.java
+++ MakeStars.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+import java.io.File;
 public class MakeStars {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String nextLn = "";
+    String ans = "";
+
+    while (sc.hasNextLine()) {
+      nextLn = sc.nextLine().toLowerCase();
+      for (int i = 0; i < nextLn.length(); i++) {
+        if (nextLn.charAt(i) >= 97 && nextLn.charAt(i) <= 122) {
+          ans += "*";
+        } else {
+          ans += nextLn.charAt(i);
+        }
+      }
+      ans = ans.trim();
+      System.out.println(ans);
+      ans = "";
+    }
+
+  }
 }
