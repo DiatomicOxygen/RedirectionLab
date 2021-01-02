42a7625ddff597435482c957bbc69c72b114a062
DiatomicOxygen <>
2021-01-02 16:20:44 -0500

    pigLatinSimple works

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,15 @@
 public class PigLatin {
   public static void main(String[] args) {
+    
+  }
 
+  public static String pigLatinSimple(String s) {
+    String ans = "";
+    if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u') {
+      ans = s + "hay";
+    } else {
+      ans = s.substring(1) + s.charAt(0) + "ay";
+    }
+    return ans;
   }
 }
