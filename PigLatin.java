43e286b75a00c060002a5f88fc6bc61066fe8611
DiatomicOxygen <>
2021-01-02 16:51:45 -0500

    pigLatin method works

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,6 @@
 public class PigLatin {
   public static void main(String[] args) {
-    
+
   }
 
   public static String pigLatinSimple(String s) {
@@ -11,5 +11,18 @@ public class PigLatin {
       ans = s.substring(1) + s.charAt(0) + "ay";
     }
     return ans;
+  }
+
+  public static String pigLatin(String s) {
+    String ans = "";
+    String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+
+    for (String diagraph : diagraphs) {
+      if (s.substring(0,2).equals(diagraph)) {
+        ans = s.substring(2) + diagraph + "ay";
+        return ans;
+      }
+    }
+    return pigLatinSimple(s);
   }
 }
